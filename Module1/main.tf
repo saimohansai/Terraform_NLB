497aba8bcf6fad0a76a211e323534eef659e7699
saimohansai <mohansai25@gmail.com>
2021-02-21 23:40:19 +0530

    issue with instace creation unsble to attach  subnets to instance

---
+++ Module1/main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Dev
+
+module "Dev_vpc" {
+  source = "./module/my_vpc"
+  vpc-cidr = "192.168.0.0/16"
+  vpc-name = "Dev-vpc"
+  Subnet-public="Dev-public"
+  Subnet-private= "Dev-private"
+  IGW_tag="Dev-IGW"
+  NAT_tag="Dev-NAT"
+  Public_rout="Dev-public-rout"
+  Private_rout="Dev-private-rout"
+  EIP_NAT = "Dev-NAT-EIP"
+  public-file = "package_install"
+
+}
+//#resource "null_resource" "before" {}
+//resource "null_resource" "delay" {
+//
+//  triggers = {
+//    "lb_dns_name" = "${module.Dev_vpc}"
+//    #"before" = "${module.Dev_vpc}"
+//  }
+//  provisioner "local-exec" {
+//    command= "sleep 300"
+//  }
+//
+//}
+//resource "null_resource" "after" {
+//depends_on = ["null_resource.delay"]
+//}
+//
+//
+module "Dev_Ec2" {
+ # depends_on = ["${modile.}""]
+  source = "./module/my_Ec2"
+  vpc_id=module.Dev_vpc.VPC-id_output
+  pub-subnet-count = module.Dev_vpc.public-subnet-count
+  public_wait=module.Dev_vpc.public-wait
+
+  #pub-subnet-ids=module.Dev_vpc.subnetids-info
+}
+//module "T" {
+//  source = ""
+//}
+//
+//#Test
+//
+//module "Testing_Env" {
+//  source = "./module/"
+//}
+//
+//#prod
+//module "Testing_Env" {
+//  source = "./module/"
+//}
---
+++ Module1/module/my_Ec2/ec2.tf
@@ -0,0 +1,71 @@
+//data "aws_subnet_ids" "subnets" {
+//  vpc_id = var.vpc_id
+//  tags = {
+//    Tier = "Public"
+//  }
+//}
+//data "aws_subnet" "subnet"{
+//for_each =
+//}
+//data "aws_subnet" "subnetids" {
+//  for_each = data.aws_subnet_ids.subnets.ids
+//  id=each.value
+//}
+
+//data "aws_subnet_ids" "public-subnet-id" {
+//  vpc_id = "${var.vpc_id}"
+//  tags = {
+//    Name= "all private-subnets"
+//  }
+//}
+  //resource "aws_instance" "private" {
+  //  #count = 1
+  //  count = length(aws_subnet.private)
+  //  ami = "${lookup(var.Myimages,"us-east-1")}"
+  //  instance_type = "t2.micro"
+  //  #availability_zone = "${element(var.azs,count.index)}"
+  //  subnet_id = aws_subnet.private[count.index].id
+  //  vpc_security_group_ids = ["${aws_security_group.Allow-all.id}"]
+  //  key_name = "Terraform"
+  //  user_data = "${file("package_install-${count.index}.sh")}"
+  //  #associate_public_ip_address = false
+  //  tags = {
+  //    Name = "private-.${count.index}"
+  //  }
+  //}
+data "aws_subnet_ids" "private_id" {
+  vpc_id = "${var.vpc_id}"
+
+  tags = {
+   Name = "private"
+  }
+}
+output "ec2_virvate" {
+  value = data.aws_subnet_ids.private_id.ids
+}
+
+  resource "aws_instance" "public" {
+    depends_on = [var.public_wait]
+    #depends_on = ["${}"]
+    #count = 3
+    #count = length("${data.aws_subnet_ids.public.ids}")
+    for_each = "${data.aws_subnet_ids.private_id}"
+    #count = var.pub-subnet-count
+    ami = "${lookup(var.Myimages,"us-east-1")}"
+    instance_type = "t2.micro"
+    #availability_zone = "${element(var.azs,count.index)}"
+    # subnet_id =data.aws_subnet_ids.public.ids[count.index]
+    subnet_id =each.value
+    #subnet_id = data.aws_subnet_ids.subnets[count.index].id
+    #subnet_id = aws_subnet.public[count.index].id
+    #vpc_security_group_ids = ["${aws_security_group.Allow-all.id}"]
+    key_name = "Terraform"
+    #user_data = "${file("module/package_install-${count.index}.sh")}"
+    #"Personal-Private-${count.index}"
+    #associate_public_ip_address = true
+    tags = {
+       #Name = "private-.${count.index}"
+        Name = "public"
+     }
+  }
+
